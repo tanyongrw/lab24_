085c4319190fb88a0f60bbc255795cb919768fa0
tanyongrw <120167717+tanyongrw@users.noreply.github.com>
2025-02-28 14:56:47 +0700

    finish

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
+
 //Write function count() here.
+int count(int data[],unsigned int size){
+	set<int> c;
+	for(unsigned int i = 0 ; i < size ; i++) c.insert(data[i]);
+	return c.size();
+}
+
+
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
