f580c40aadf1f03294fff0968ec0a2663c5281e8
tanyongrw <120167717+tanyongrw@users.noreply.github.com>
2025-02-28 14:54:54 +0700

    finish

--- lab24_1.cpp
+++ lab24_1.cpp
@@ -63,4 +63,17 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int x){
+    size--;
+	Node *N  = root;
+	if(x == 0){
+		root = root -> next;
+		delete N;
+	}
+	else{
+		for(int i = 0; i < x-1; i++) N = N -> next;
+		Node *M  = N -> next;
+		N -> next = M -> next;
+		delete M;
+	}
+}
